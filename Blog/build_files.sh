1375ce806e25ecfdb96aa7a191711e10b0bdaa33
M Sharjeel Shaikh <shaikhsharjeel02@gmail.com>
2023-01-11 00:33:45 +0500

    Change in setting

--- Blog/build_files.sh
+++ Blog/build_files.sh
@@ -1,5 +1,5 @@
 
 echo = "BUILD START"
-python3.7.5 -m pip install -r requirment.txt
+python3.7.5 -m pip install -r requirments.txt
 python3.7.5 manage.py collectstatic  --noinput --clear
 echo = "BUILD END"
